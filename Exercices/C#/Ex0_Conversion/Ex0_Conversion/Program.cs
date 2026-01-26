1dbba7d365c38cf767b413807d338fad5325c7e6
Karol Stopa <Kstopa6@gmail.com>
2026-01-26 08:53:58 +0100

    Exo C -> C#

--- Exercices/C#/Ex0_Conversion/Ex0_Conversion/Program.cs
+++ Exercices/C#/Ex0_Conversion/Ex0_Conversion/Program.cs
@@ -1,92 +1,86 @@
 ﻿// Caneva Ex1 SL228_POBJ
 // Conversion C -> C# (console)
 
-using System;
+using System; // #include <stdio.h>
 
-class Program
+class Program // Wrapper de class pour C#
 {
-    static void Main(string[] args)   // C : int main(void)
+    static void Main() // int main (void)
     {
         char UserAnswer;
         int ValA, ValB;
+        // Variables pour test A et B
         short i;
         short ValB1;
         double ValB2;
 
-        Console.WriteLine("Exercice 1 : Huber Christian ");   // C : printf(...)
-        Console.WriteLine("Solution en C a trandformer en C++ "); // C : printf(...)
+        Console.WriteLine("Exercice 1 : Huber Christian"); // printf("Exercice 1 : Huber Christian \n");
+        Console.WriteLine("Solution en C a trandformer en C++"); // printf("Solution en C a trandformer en C++ \n");
 
         do
         {
-            Console.WriteLine("Test A ou B, Q pour Quitter "); // C : printf(...)
-
-            string input = Console.ReadLine();                // C : scanf_s("%c%*c", &UserAnswer, 2)
-            UserAnswer = (input != null && input.Length > 0) ? input[0] : '\0'; // récupération du char
+            Console.WriteLine("Test A ou B, Q pour Quitter"); // printf("Test A ou B, Q pour Quitter \n");
+            UserAnswer = Console.ReadKey().KeyChar; // scanf_s("%c%*c", &UserAnswer, 2);
+            Console.WriteLine(); // Saut de ligne après ReadKey
 
             switch (UserAnswer)
             {
                 case 'A':
                 case 'a':
-                    Console.WriteLine("TestA: entrez un nombre entre 1 et 9 "); // C : printf(...)
-
-                    if (!int.TryParse(Console.ReadLine(), out ValA))            // C : scanf_s("%d%*c", &ValA)
-                        ValA = 0;
+                    Console.WriteLine("TestA: entrez un nombre entre 1 et 9"); // printf("TestA: entrez un nombre entre 1 et 9 \n");
+                    ValA = int.Parse(Console.ReadLine()); // scanf_s("%d%*c", &ValA);
 
                     if (ValA > 9)
                     {
                         ValA = 9;
-                        Console.WriteLine("TestA: ValA limitee a 9 ");          // C : printf(...)
+                        Console.WriteLine("TestA: ValA limitee a 9"); // printf("TestA: ValA limitee a 9 \n");
                     }
+
                     if (ValA == 0)
                     {
                         ValA = 1;
-                        Console.WriteLine("TestA: ValA forcee a 1 ");           // C : printf(...)
+                        Console.WriteLine("TestA: ValA forcee a 1"); // printf("TestA: ValA forcee a 1 \n");
                     }
+
                     if (ValA > 0)
                     {
-                        Console.Write(ValA + " ");                              // C : printf("%d ", ValA)
+                        Console.Write("{0} ", ValA); // printf("%d ", ValA);
 
                         for (i = 0; i < ValA; i++)
                         {
-                            Console.Write("*");                                 // C : printf("*")
+                            Console.Write("*"); // printf ("*");
                         }
 
-                        Console.WriteLine();                                    // C : printf("\n")
+                        Console.WriteLine(); // printf("\n");
                     }
                     else
                     {
-                        Console.WriteLine("TestA: ValA est negatif ! ");        // C : printf(...)
+                        Console.WriteLine("TestA: ValA est negatif !"); // printf("TestA: ValA est negatif ! \n");
                     }
                     break;
 
                 case 'B':
                 case 'b':
-                    Console.WriteLine("TestB: entrez une valeur entre 0 et 9 "); // C : printf(...)
-
-                    if (!int.TryParse(Console.ReadLine(), out ValB))             // C : scanf_s("%d%*c", &ValB)
-                        ValB = -1;
+                    Console.WriteLine("TestB: entrez une valeur entre 0 et 9"); // printf("TestB: entrez une valeur entre 0 et 9 \n");
+                    ValB = int.Parse(Console.ReadLine()); // scanf_s("%d%*c", &ValB);
 
                     if (ValB >= 0 && ValB <= 9)
                     {
                         for (i = 0; i < ValB; i++)
                         {
-                            ValB1 = (short)(100 + (10 * i));                    // cast short (C# obligatoire)
+                            ValB1 = (short)(100 + (10 * i)); // ValB1 = 100 + (10 * i);
                             ValB2 = ValB1 / 10000.0;
-
-                            Console.WriteLine(                                   // C : printf(...)
-                                "TestB: i= {0} ValB1 = {1} ValB2 = {2}",
-                                i, ValB1, ValB2);
+                            Console.WriteLine("TestB: i= {0} ValB1 = {1} ValB2 = {2}", i, ValB1, ValB2); // printf("TestB: i= %d ValB1 = %d ValB2 = %lf \n", i, ValB1, ValB2);
                         }
                     }
                     else
                     {
-                        Console.WriteLine("TestB: ValB n'est pas entre 0 et 9 ! "); // C : printf(...)
+                        Console.WriteLine("TestB: ValB n'est pas entre 0 et 9 !"); // printf("TestB: ValB n'est pas entre 0 et 9 ! \n");
                     }
                     break;
             }
 
         } while (!(UserAnswer == 'Q' || UserAnswer == 'q'));
 
-        // C : return(0)
-    }
+    } // return (0);
 }
--- Exercices/C#/Ex0_Conversion/Ex0_Conversion/obj/Debug/net6.0/Ex0_Conversion.AssemblyInfo.cs
+++ Exercices/C#/Ex0_Conversion/Ex0_Conversion/obj/Debug/net6.0/Ex0_Conversion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex0_Conversion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+024cc1a9440192cc592636fb95c9653c0ae68463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80edcef41013f63ea861c5786a0a88fa22d2a2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex0_Conversion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex0_Conversion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
